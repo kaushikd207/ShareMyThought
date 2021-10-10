067587066862af32b3f9256f5b53c70d1d819702
kaushikd207 <kaushikd207@gmail.com>
2021-10-10 22:19:12 +0530

    modifid

---
+++ src/Navbar.js
@@ -0,0 +1,15 @@
+const Navbar = () => {
+    return ( 
+        <nav className="nabar">
+            <h1>ShareMyThought</h1>
+            <div className="links">
+                <a href="/">Home</a>
+                <a href="/reate">New Blog</a>
+            </div>
+        </nav>
+
+
+     );
+}
+ 
+export default Navbar;
