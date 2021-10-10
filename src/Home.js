f2264d13adea2682ecd97911ba46957655fd8db4
kaushikd207 <kaushikd207@gmail.com>
2021-10-10 22:56:46 +0530

    added

--- src/Home.js
+++ src/Home.js
@@ -1,7 +1,11 @@
 const Home = () => {
+    const handleClick = () =>{
+        console.log("Button clicked");
+    }
     return (  
         <div className="home">
             <h2>HomePage</h2>
+            <button onClick={handleClick}>click</button>
         </div>
     );
 }
--- src/Navbar.js
+++ src/Navbar.js
@@ -1,15 +1,17 @@
 const Navbar = () => {
-    return ( 
-        <nav className="nabar">
-            <h1>ShareMyThought</h1>
-            <div className="links">
-                <a href="/">Home</a>
-                <a href="/reate">New Blog</a>
-            </div>
-        </nav>
-
-
-     );
-}
- 
-export default Navbar;
+    return (
+      <nav className="navbar">
+        <h1>ShareMyThought</h1>
+        <div className="links">
+          <a href="/">Home</a>
+          <a href="/create" style={{ 
+            color: 'white', 
+            backgroundColor: '#f1356d',
+            borderRadius: '8px' 
+          }}>New Blog</a>
+        </div>
+      </nav>
+    );
+  }
+   
+  export default Navbar;
